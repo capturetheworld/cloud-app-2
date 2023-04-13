492ebd41e92f3e53df6ee25f26f4f3153458f187
CloudApartments <122237622+CloudApartments@users.noreply.github.com>
2023-04-13 13:40:54 -0700

    thumb location on sliders responding better

--- cloud/components/CloudSlider.tsx
+++ cloud/components/CloudSlider.tsx
@@ -3,21 +3,36 @@ import Box from '@mui/material/Box'
 import Slider from '@mui/joy/Slider'
 import styled from '@emotion/styled'
 
-export default function VerticalSlider() {
+
+interface Props {
+  valueLabelDisplay: boolean,
+  icon?: string,
+}
+export default function VerticalSlider({ valueLabelDisplay }: Props) {
   function preventHorizontalKeyboardNavigation(event: React.KeyboardEvent) {
     if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
       event.preventDefault()
     }
   }
+  const icon = require('/assets/adaptive-icon.png')
+  console.log("ICON", icon)
+  const [sliderValue, setSliderValue] = React.useState(0)
 
+  const onChange = (event, newValue) => {
+    console.log('Slider value changed to', newValue)
+    setSliderValue(newValue)
+  }
   return (
     <Box sx={{ height: 300 }}>
       <StyledSlider
-        color="danger"
+        // color="danger"
+        icon={icon}
         marks={false}
         orientation="vertical"
         size="lg"
-        valueLabelDisplay="on"
+        sliderValue={sliderValue}
+        onChange={onChange}
+        valueLabelDisplay={valueLabelDisplay ? "on" : "off"}
         variant="plain"
         sx={{
           "--Slider-track-size": "21px",
@@ -25,7 +40,8 @@ export default function VerticalSlider() {
           "--Slider-thumb-size": "0px",
           "--Slider-thumb-width": "0px",
           "--Slider-valueLabel-arrowSize": "0px",
-          backGroundColor: 'red'
+          backGroundColor: 'red',
+          borderRadius: '14px',
         }}
       />
     </Box>
@@ -35,23 +51,37 @@ export default function VerticalSlider() {
 
 
 
-const StyledSlider = styled(Slider)<any>(() => ({
-  backgroundColor: '#D5D4CD',
-  borderRadius: '14px',
-  '& .MuiSlider-track': {
-    backgroundColor: '#FFFFFF',
-    borderRadius: '2px',
-    width: '80%',
-    maxHeight: '100%',
-  },
-  '& .MuiSlider-rail': {
-    // backgroundColor: '#00FF00',
-    borderRadius: '2px',
-    width: '80%',
-    maxHeight: '100%',
-  },
-  '& .MuiSlider-thumb': {
-    backgroundColor: 'purple',
-    borderRadius: '8px',
-  },
-}))
+const StyledSlider = styled(Slider, {
+  shouldForwardProp: (prop) => prop !== 'valueLabelDisplay'
+})<any>((prop) => {
+  return ({
+    backgroundColor: '#D5D4CD',
+    borderRadius: '14px',
+    width: "70px",
+    '& .MuiSlider-track': {
+      backgroundColor: "#FFFFFF",
+      borderRadius: "12px",
+      width: "80%",
+      maxHeight: "97%",
+      marginBottom: "3.5px",
+      marginTop: "3.5px"
+    },
+    '& .MuiSlider-rail': {
+      borderRadius: "12px",
+      width: "80%",
+      maxHeight: "97%",
+      marginBottom: "3.5px",
+      marginTop: "3.5px"
+    },
+    '& .MuiSlider-thumb': {
+      backgroundColor: 'transparent',
+      border: 'none',
+      backgroundImage: `url(${prop.icon})`,
+      marginBottom: `${(-prop.sliderValue * .5) + 21}px`,
+      marginTop: "3.5px",
+    },
+    '& .MuiSlider-slider': {
+    },
+  })
+}
+)
